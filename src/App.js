de1bbf6f948890dac5aad1d13ad3c4cbfd333067
Josiah Parkhill <josiahparkhill@josiahs-mbp-3.mynetworksettings.com>
2022-12-04 22:22:37 -0500

    Added components folder, moved stylesheets into public folder, removed base React code from app.js and index.js to prepare for donut car shop code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { VehicleProvider } from './context/VehicleProvider';
+import { UserProvider } from './context/UserProvider';
+import '../public/stylesheets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <VehicleProvider>
+        <div>
+          <p>This is the framework for the home page.</p>
+        </div>
+      </VehicleProvider>
+    </UserProvider>
   );
 }
 
