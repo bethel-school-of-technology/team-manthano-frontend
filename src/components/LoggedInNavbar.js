c81f32d1474434921b4a3e4a401655d6b866d30d
Josiah Parkhill <josiahparkhill@Josiahs-MBP-3.home>
2022-12-10 12:29:25 -0500

    assigned sticky attribute to navbar

--- src/components/LoggedInNavbar.js
+++ src/components/LoggedInNavbar.js
@@ -8,7 +8,7 @@ const LoggedInNavbar = () => {
     const { user } = useContext(UserContext);
 
     return (
-        <Navbar className={styles.navbar} expand="lg" variant="dark">
+        <Navbar className={styles.navbar} expand="lg" variant="dark" sticky="top">
             <Container>
                 <h1 className={styles.brand}><i className="bi bi-record-circle-fill" style={{ 'color': '#FFD6E0' }}></i> DONUT CAR SHOP</h1>
                 <Nav>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './public/stylesheets/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
