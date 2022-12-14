a335db46ae413a82e7897ed730b59acf6c9c051b
Jacob Imig <jacobimig9@gmail.com>
2022-12-14 12:32:17 -0600

    Added CRUD Functions to Vehicle Provider

--- src/context/VehicleProvider.js
+++ src/context/VehicleProvider.js
@@ -5,7 +5,7 @@ import VehicleContext from "./VehicleContext";
 export const VehicleProvider = (props) => {
 
     const [vehicle, setVehicle] = useState([]);
-    const baseUrl = "http://localhost:3000/api";
+    const baseUrl = "http://localhost:3000/api/vehicles/";
 
     useEffect(() => {
         async function fetchData() {
@@ -15,23 +15,49 @@ export const VehicleProvider = (props) => {
     }, []);
 
     function getAllVehicles() {
-
+        return axios.get(baseUrl).then(response => setVehicle(response.data));
     }
 
-    function getSingleVehicle() {
-
+    function getSingleVehicle(id) {
+        return axios.get(`${baseUrl}/${id}`).then(response => new Promise((resolve) => resolve(response.data)))
+            .catch((error) => console.log(error));
     }
 
-    function addVehicle() {
+    function addVehicle(single_vehicle) {
+        let myHeaders = {
+            Authorization: `Bearer ${localStorage.getItem('myVehicleToken')}`
+        };
 
+        return axios.post(baseUrl, single_vehicle, { headers: myHeaders })
+            .then(response => {
+                getAllVehicles();
+                return new Promise(resolve => resolve(response.data));
+            }
+            );
     }
 
     function editVehicle() {
-
+        let myHeaders = {
+            Authorization: `Bearer ${localStorage.getItem('myVehicleToken')}`
+        };
+        return axios.put(`${baseUrl}/${post._id}`, post, { headers: myHeaders })
+            .then(response => {
+                getAllVehicles();
+                return new Promise(resolve => resolve(response.data));
+            }
+            );
     }
 
     function deleteVehicle() {
-
+        let myHeaders = {
+            Authorization: `Bearer ${localStorage.getItem('myVehicleToken')}`
+        };
+        return axios.delete(`${baseUrl}/${post._id}`, { headers: myHeaders })
+            .then(response => {
+                getAllVehicles();
+                return new Promise(resolve => resolve(response.data));
+            }
+            );
     }
 
     return (
