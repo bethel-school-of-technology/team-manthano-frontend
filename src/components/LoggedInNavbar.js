69c220adc7b5e757e24643ae4d58cc7a9c8224d5
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-01-09 13:26:23 -0500

    fixed navigation rendering and routing as well as token and currentUser

--- src/components/LoggedInNavbar.js
+++ src/components/LoggedInNavbar.js
@@ -1,13 +1,18 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import styles from '../public/stylesheets/Navbar.module.css';
+import { useLocation, useNavigate } from "react-router";
 
 const LoggedInNavbar = () => {
 
+    let location = useLocation();
+    let navigate = useNavigate();
+
     const [loggedInUser, setLoggedInUser] = useState({});
+
     useEffect(() => {
         setLoggedInUser(JSON.parse(localStorage.getItem('currentUser')))
-    }, [])
+    }, [location])
 
     return (
         <Navbar className={styles.navbar} expand="lg" variant="dark" sticky="top">
@@ -18,6 +23,8 @@ const LoggedInNavbar = () => {
                     <button className={styles.button} onClick={(e) => {
                         e.preventDefault()
                         localStorage.removeItem("userToken")
+                        localStorage.removeItem("currentUser")
+                        window.location.reload('/')
                         this.props.history.push('/')
                     }}>LOG OUT</button>
                 </Nav>
