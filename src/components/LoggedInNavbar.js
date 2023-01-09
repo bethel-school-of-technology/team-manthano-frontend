62abdc18bb1cbb738d0a8ca601400d8fab3c51ef
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-01-09 13:31:44 -0500

    deleted redundancy

--- src/components/LoggedInNavbar.js
+++ src/components/LoggedInNavbar.js
@@ -1,18 +1,14 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import styles from '../public/stylesheets/Navbar.module.css';
-import { useLocation, useNavigate } from "react-router";
 
 const LoggedInNavbar = () => {
-
-    let location = useLocation();
-    let navigate = useNavigate();
 
     const [loggedInUser, setLoggedInUser] = useState({});
 
     useEffect(() => {
         setLoggedInUser(JSON.parse(localStorage.getItem('currentUser')))
-    }, [location])
+    }, [])
 
     return (
         <Navbar className={styles.navbar} expand="lg" variant="dark" sticky="top">
