8c16f2d6b8b5b0477f6ed92feccf3de47daf0bb2
JHuffey <huffey@gmail.com>
2023-01-16 11:26:04 -0600

    link to userAccount page when clicking on username in loggedInNavbar

--- src/components/LoggedInNavbar.js
+++ src/components/LoggedInNavbar.js
@@ -2,6 +2,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import styles from '../public/stylesheets/Navbar.module.css';
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const LoggedInNavbar = () => {
 
@@ -24,7 +25,7 @@ const LoggedInNavbar = () => {
                     <h1 className={styles.brand}><i className="bi bi-record-circle-fill" style={{ 'color': '#FFD6E0' }}></i> DONUT CAR SHOP</h1>
                 </div>
                 <Nav>
-                    <p className={styles.greeting}>HELLO, <a className={styles.user} href={`/profile/${loggedInUser._id}`}>{loggedInUser.username}</a></p>
+                    <p className={styles.greeting}>HELLO, <Link className={styles.user} to="/account">{loggedInUser.username}</Link></p>
                     <button className={styles.button} onClick={(e) => {
                         e.preventDefault()
                         localStorage.removeItem("userToken")
