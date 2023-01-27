1a655c865ddee977a5f61c153f30ef51fc1c5fd4
kelee <keleesmith69@gmail.com>
2023-01-27 14:57:27 -0500

    updated info

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -46,6 +46,34 @@ const Details = ({ getSingleVehicle }) => {
             <h3>Condition</h3>
             {vehicle.Condition}
           </div>
+          <div>
+          <h3>Year</h3>
+            {vehicle.Year}
+          </div>
+          <div>
+          <h3>Name</h3>
+            {vehicle.Name}
+          </div>
+          <div>
+            <h3>Body_Style</h3>
+            {console.log(vehicle.Body_Style)}
+          </div>
+          <div>
+            <h3>Vehicle_Manufacturer</h3>
+            {console.log(vehicle.Vehicle_Manufacturer)}
+          </div>
+          <div>
+            <h3>Vehicle_Model</h3>
+            {console.log(vehicle.Vehicle_Model)}
+          </div>
+          <div>
+            <h3>Fuel_Economy</h3>
+            {console.log(vehicle.Fuel_Economy)}
+          </div>
+          <div>
+            <h3> Exterior_Color</h3>
+            {console.log(vehicle.Exterior_Color)}
+          </div>
         </div>
       </div>
     </div>
