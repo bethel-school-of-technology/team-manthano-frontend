2b92e00b65ce662986d30e7a1c425c80b6f5382e
Jacob Imig <jacobimig9@gmail.com>
2023-01-29 13:27:51 -0600

    Updated bugs and added Map to Detail page

--- src/components/MapsAPI.js
+++ src/components/MapsAPI.js
@@ -1,33 +1,36 @@
-// import React, { useContext, useEffect, useState } from 'react'
-// import imgstyles from '../assets/css/img.css'
-// import {
-//  StaticGoogleMap,
-//  Marker
-// } from 'react-static-google-map';
-// import UserContext from '../context/UserContext'
+import React, { useContext, useEffect, useState } from 'react'
+import imgstyles from '../assets/css/img.css'
+import {
+ StaticGoogleMap,
+ Marker
+} from 'react-static-google-map';
+import UserContext from '../context/UserContext'
 
-// const MapsAPI = (props) => {
-//  const { getOneUser } = useContext(UserContext)
-//  const { postedBy } = props;
-//  const [userZip, setUserZip] = useState(null)
+const MapsAPI = (props) => {
+ const { getOneUser } = useContext(UserContext)
+ const { postedBy } = props;
+ const [userZip, setUserZip] = useState(null)
 
-//  useEffect(() => {
-//   getOneUser(postedBy).then(res => {
-//    setUserZip(res?.zip)
-//   }).catch(error => console.log("ERROR: ", error))
-//  }, [])
+ useEffect(() => {
+  getOneUser(postedBy).then(res => {
+   setUserZip(res?.zip)
+  }).catch(error => console.log("ERROR: ", error))
+ }, [])
 
-//  if (!userZip) {
-//   return;
-//  }
+ if (!userZip) {
+  return;
+ }
 
-//  return (
-//   <StaticGoogleMap size="600x600" apiKey="AIzaSyDRXjXx77oZ6bA2rk3NJ6K5ZkEQ8gYP2DM" className="map">
-//    <Marker.Group label="" color="red">
-//     <Marker location={userZip} />
-//    </Marker.Group>
-//   </StaticGoogleMap>
-//  );
-// }
+ return (
+  <section className="user-map">
+   <p>Seller's location</p>
+   <StaticGoogleMap size="500x500" apiKey="AIzaSyDRXjXx77oZ6bA2rk3NJ6K5ZkEQ8gYP2DM" className="map">
+    <Marker.Group label="" color="red">
+     <Marker location={userZip} />
+    </Marker.Group>
+   </StaticGoogleMap>
+  </section>
+ );
+}
 
-// export default MapsAPI
+export default MapsAPI
